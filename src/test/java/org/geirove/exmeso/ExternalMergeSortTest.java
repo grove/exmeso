877d3fff414a836d1c5e5b0fbd67e1c7f495bae5
Geir Ove Grønmo <grove@geirove.org>
2013-09-21 15:23:58 +0200

    Test now actually a test.

--- src/test/java/org/geirove/exmeso/ExternalMergeSortTest.java
+++ src/test/java/org/geirove/exmeso/ExternalMergeSortTest.java
@@ -1,9 +1,13 @@
 package org.geirove.exmeso;
 
+import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
 
 import org.codehaus.jackson.annotate.JsonProperty;
@@ -24,7 +28,23 @@ public class ExternalMergeSortTest {
         public String toString() {
             return "{\"value\":" + value + "\"}";
         }
+        public boolean equals(Object o) {
+            if (o instanceof StringPojo) {
+                StringPojo other = (StringPojo)o;
+                return other.value.equals(value);
+            }
+            return false;
+        }
+        public int hashCode() {
+            return value.hashCode();
+        }
     }
+
+    private static final StringPojo A = new StringPojo("A");
+    private static final StringPojo B = new StringPojo("B");
+    private static final StringPojo C = new StringPojo("C");
+    private static final StringPojo D = new StringPojo("D");
+    private static final StringPojo E = new StringPojo("E");
 
     @Test
     public void test() throws IOException {
@@ -34,19 +54,35 @@ public class ExternalMergeSortTest {
                 return o1.getValue().compareTo(o2.getValue());
             }
         };
-        JacksonSort<StringPojo> writer = new JacksonSort<StringPojo>(comparator, StringPojo.class);
-        ExternalMergeSort<StringPojo> sort = new ExternalMergeSort<StringPojo>(writer, new File("/tmp"), 2);
+        JacksonSort<StringPojo> handler = new JacksonSort<StringPojo>(comparator, StringPojo.class);
+        String tmpdir = System.getProperty("java.io.tmpdir");
+        
+        ExternalMergeSort<StringPojo> sort = new ExternalMergeSort<StringPojo>(handler, new File(tmpdir), 2);
 
-        List<StringPojo> chunk = Arrays.<StringPojo>asList(new StringPojo("B"), new StringPojo("D"), new StringPojo("E"), new StringPojo("C"), new StringPojo("A"));
+        List<StringPojo> expected = Arrays.<StringPojo>asList(A, B, C, D, E);
+        List<StringPojo> input = Arrays.<StringPojo>asList(B, D, E, C, A);
 
-        MergeIterator<StringPojo> iter = sort.mergeSort(chunk.iterator());
+        MergeIterator<StringPojo> result = sort.mergeSort(input.iterator());
+        assertResult(result, expected);
+    }
+
+    private void assertResult(MergeIterator<StringPojo> result, List<StringPojo> expected) throws IOException {
+        Iterator<StringPojo> iter = expected.iterator();
         try {
-            while (iter.hasNext()) {
-                StringPojo o = iter.next();
-                System.out.println("O: " + o);
+            while (result.hasNext()) {
+                StringPojo r = result.next();
+                System.out.println("O: " + r);
+                if (!iter.hasNext()) {
+                    fail("More than expected");
+                }
+                StringPojo e = iter.next();
+                assertEquals(r, e);
+            }
+            if (iter.hasNext()) {
+                fail("Fewer than expected");
             }
         } finally {
-            iter.close();
+            result.close();
         }
     }
 
